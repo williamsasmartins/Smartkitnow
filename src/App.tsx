1ab608eedb283b0493a45e0f7de913faa80a36dc
Williams Martins <williamsasmartins@gmail.com>
2025-09-25 15:04:24 -0700

    Removido duplicado de IMCCalculator no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import IMCCalculator from '@/components/calculators/IMCCalculator';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
