fed50abec25ae1d9cd04b58a3f29c0af092e6b81
Williams Martins <williamsasmartins@gmail.com>
2025-11-19 15:19:49 -0800

    feat: add LoanPaymentV2Calculator component

---
+++ src/components/calculators/Financial/LoanPaymentV2Calculator.tsx
@@ -0,0 +1,3 @@
+I see you've sent just three dots. That could mean a few different things - maybe you're thinking, feeling uncertain about what to say, or just testing if I'm here. 
+
+I'm here and ready to help with whatever you'd like to talk about or any questions you might have. What's on your mind?
