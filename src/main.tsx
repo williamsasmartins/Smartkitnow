6926f58f65f16e91884ba97de2b65859e3c63b7a
Williams Martins <williamsasmartins@gmail.com>
2026-01-20 20:42:23 -0800

    main

--- src/main.tsx
+++ src/main.tsx
@@ -42,17 +42,23 @@ if (ENABLE_SPEED_INSIGHTS && !IS_LOCAL && !IS_SMARTKIT_DOMAIN) {
     .catch(() => { /* ignore */ })
 }
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from "@/components/ui/sonner"
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Provider de tema no topo do app */}
-    <ThemeProvider attribute="class" defaultTheme="dark">
-      <HelmetProvider>
-        <BrowserRouter basename="/">
-          {/* Renderiza o App diretamente (sem Sentry.ErrorBoundary em localhost) */}
-          <App />
-        </BrowserRouter>
-      </HelmetProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <HelmetProvider>
+          <BrowserRouter basename="/">
+            <App />
+            <Toaster />
+          </BrowserRouter>
+        </HelmetProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
 
